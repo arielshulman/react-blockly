c273cbede118edf9bded04f8ece3dd23f1c3c7e2
Ariel Shulman <shulmanariel@gmail.com>
2024-01-21 20:24:24 +0200

    DefaultProps is deprecated.

--- src/BlocklyWorkspace.tsx
+++ src/BlocklyWorkspace.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import useBlocklyWorkspace from "./useBlocklyWorkspace";
-import {BlocklyWorkspaceProps} from "./BlocklyWorkspaceProps";
+import { BlocklyWorkspaceProps } from "./BlocklyWorkspaceProps";
 
 const propTypes = {
   initialXml: PropTypes.string,
